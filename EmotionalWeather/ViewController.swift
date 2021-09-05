53b2ba1c22299e0eff3e642daeb94dc781b5b51b
I.D <daisuke.workin@gmail.com>
2021-09-05 15:10:57 +0900

    initialize

---
+++ EmotionalWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmotionalWeather
+//
+//  Created by 石澤大輔 on 2021/09/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
